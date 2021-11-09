320edf6aed0572bd23dc2c5b312b4fd1beed7c36
Alex Sweeney <alexsweeney@live.co.uk>
2021-11-09 16:42:17 +0700

    feat(App.jsx) background added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
+import './reset.css';
 import './App.css';
+import backgroundImage from './imgs/background-image.jpg';
 
 function App() {
   return (
-     <div></div>
+     <div className="container">
+     	<section className="main-view white-background">
+     		<div className="main-view-left"></div>
+     		<div className="main-view-right">
+     			<header>
+
+     			</header>
+     			<img src={backgroundImage} alt="background-image"/>
+     		</div> 
+     	</section>
+     	<section className="alert-bar pink-background" alt="background-image">
+
+     	</section>
+     	<section className="it-cloud-view icon-view white-background">
+
+     	</section>
+     	<section className="accounting-view icon-view grey-background">
+
+     	</section>
+     	<section className="hr-admin-view icon-view white-background">
+
+     	</section>
+     	<section className="sales-view icon-view grey-background">
+
+     	</section>
+     	<section className="customer-view icon-view white-background">
+
+     	</section>
+     	<section className="security-view icon-view grey-background">
+
+     	</section>
+     	<section className="options-view blue-background">
+
+     	</section>
+     	<section className="offices-view blue-background">
+
+     	</section>
+     </div>
   );
 }
 
