58ade60c6a724cb2f8ab5d582f593cde602bfa2e
Alex Sweeney <alexsweeney@live.co.uk>
2021-11-13 06:16:18 +0700

    feat(MainView.jsx) style for very large screens

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   return (
   	<div className="container"> 
       <Header/>
-    	{/*<MainView/>*/}
+    	<MainView/>
       {/*<AlertBar/>
       <ServicesView/>
       <PriceOptionsView/>
--- src/components/MainView.jsx
+++ src/components/MainView.jsx
@@ -13,35 +13,32 @@ import {
 export default function MainView() {  
   return (
     <>
-      <section className="main-view-container"> 
+      <section className="main-view"> 
+        <div className="main-view-left" style={mainViewLeftStyle}>   
+          <h2 className="main-view-left-header" style={mainViewLeftHeaderStyle}>Asia IT Market Place</h2>
+        </div>
        
-        <section className="main-view"> 
-          <div className="main-view-left" style={mainViewLeftStyle}>   
-            <h2 className="main-view-left-header" style={mainViewLeftHeaderStyle}>Asia IT Market Place</h2>
-          </div>
-         
-          <div className="main-view-right" style={mainViewRightStyle}>
-           <div className="main-view-buttons">
-              <button className="main-view-button main-view-button-selected" style={mainViewButtonSelectedStyle}>Now</button>
-              <button className="main-view-button main-view-button-deselected" style={mainViewButtonDeselectedStyle}>Future</button>
-            </div> 
-             <div className="main-view-info-container">
-              <div className="main-view-info">
-                <h3 className="main-view-info-number" style={mainViewInfoNumberStyle}>6</h3>
-                <h3 className="main-view-info-text" style={mainViewInfoTextStyle}>different IT Software Networks</h3>
-              </div>
-              <div className="main-view-info">
-                <h3 className="main-view-info-number" style={mainViewInfoNumberStyle}>32</h3>
-                <h3 className="main-view-info-text" style={mainViewInfoTextStyle}>partners</h3>
-              </div>
-              <div className="main-view-info">
-                <h3 className="main-view-info-number" style={mainViewInfoNumberStyle}>4</h3>
-                <h3 className="main-view-info-text" style={mainViewInfoTextStyle}>countries</h3>
-              </div>
-            </div>
+        <div className="main-view-right" style={mainViewRightStyle}>
+         <div className="main-view-buttons">
+            <button className="main-view-button main-view-button-selected" style={mainViewButtonSelectedStyle}>Now</button>
+            <button className="main-view-button main-view-button-deselected" style={mainViewButtonDeselectedStyle}>Future</button>
           </div> 
-        </section>  
-      </section>
+           <div className="main-view-info-container">
+            <div className="main-view-info">
+              <h3 className="main-view-info-number" style={mainViewInfoNumberStyle}>6</h3>
+              <h3 className="main-view-info-text" style={mainViewInfoTextStyle}>different IT Software Networks</h3>
+            </div>
+            <div className="main-view-info">
+              <h3 className="main-view-info-number" style={mainViewInfoNumberStyle}>32</h3>
+              <h3 className="main-view-info-text" style={mainViewInfoTextStyle}>partners</h3>
+            </div>
+            <div className="main-view-info">
+              <h3 className="main-view-info-number" style={mainViewInfoNumberStyle}>4</h3>
+              <h3 className="main-view-info-text" style={mainViewInfoTextStyle}>countries</h3>
+            </div>
+          </div>
+        </div> 
+      </section>  
     </>
   )
 }
