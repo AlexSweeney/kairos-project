454ae18a05fbca3668dc5227cb3e117a86531de7
Alex Sweeney <alexsweeney@live.co.uk>
2021-11-12 18:40:24 +0700

    test(PriceOptionsView.jsx) added tests -> make sure meets figma specs

--- src/styles/PriceOptionsView.js
+++ src/styles/PriceOptionsView.js
@@ -1,63 +1,90 @@
 import { 
 	whiteBackground,
   lightBlueBackground,
+  pinkBackground,
   lightGreyBorder,
+  pinkBorder,
+  lightGreyBoxShadow,
+  whiteText,
+  darkBlueText,
 } from './colors.js';
 
+// ================= Background
 export const priceOptionsViewStyle = {
 	background: lightBlueBackground,
 };
 
+// ================= Boxes
 export const priceOptionsBoxStyle = {
 	width: '264px',
 	height: '228px',
-	padding: '42px 29px',	
 	borderRadius: '10px',
 	background: whiteBackground,
 	border: `1px solid ${lightGreyBorder}`,
-	boxShadow: '0px 13px 19px 0px #00000012',
+	boxShadow: `0px 13px 19px 0px ${lightGreyBoxShadow}`, 
 };
 						 
 export const priceOptionsBorderBoxStyle = {
 	...priceOptionsBoxStyle,
-	border: '2px solid #E6367E',
+	border: `2px solid ${pinkBorder}`,
 };
 
 export const priceOptionsHighlightBoxStyle = {
 	...priceOptionsBoxStyle,
-	background: '#E6367E',
-	color: '#FFFFFF',
+	background: pinkBackground,
+	color: whiteText,
 };
 
+// ================= Heading
 export const priceOptionBoxHeadingStyle = {
 	fontFamily: 'Roboto, sans-serif',
 	fontSize: '22px',
 	fontStyle: 'normal',
 	fontWeight: '700',
 	lineHeight: '33px',
-	letterSpacing: '0.10000000149011612px',
+	letterSpacing: '0.1px',
 	textAlign: 'left',
+	color: darkBlueText,
 };
 
+export const priceOptionBoxHighlightHeadingStyle = {
+	...priceOptionBoxHeadingStyle,
+	color: '',
+};
+
+// ================= SubHeading
 export const priceOptionBoxSubHeadingStyle = {
 	fontFamily: 'Roboto, sans-serif',
 	fontSize: '12px',
 	fontStyle: 'normal',
 	fontWeight: '700',
 	lineHeight: '18px',
-	letterSpacing: '0.10000000149011612px',
+	letterSpacing: '0.1px',
 	textAlign: 'left',
+	color: '#374754',
 };
 
+export const priceOptionBoxHighlightSubHeadingStyle = {
+	...priceOptionBoxSubHeadingStyle,
+	color: '',
+};
+
+// ================= Price
 export const priceNumberStyle = {
 	fontFamily: 'Noto Sans, sans-serif',
 	fontSize: '36px',
 	fontStyle: 'normal',
 	fontWeight: '700',
 	lineHeight: '66px',
-	letterSpacing: '0.20000000298023224px',
+	letterSpacing: '0.2px',
 	textAlign: 'left',
 	paddingRight: '5px',
+	color: '#252B42',
+}
+
+export const priceNumberHighlightStyle = {
+	...priceNumberStyle,
+	color: '',
 }
 
 export const priceZeroStyle = {
@@ -65,22 +92,34 @@ export const priceZeroStyle = {
 	fontSize: '64px',
 }; 
 
-export const priceDollarStyle = {
+export const priceUnitStyle = {
 	fontFamily: 'Roboto, sans-serif',
 	fontSize: '22px',
 	fontStyle: 'normal',
 	fontWeight: '700',
 	lineHeight: '33px',
-	letterSpacing: '0.10000000149011612px',
+	letterSpacing: '0.1px',
 	textAlign: 'left',
+	color: '#252B42',
 }; 
 
-export const priceTextStyle = {
+export const priceUnitHightLightStyle = {
+	...priceUnitStyle,
+	color: '',
+};
+
+export const priceLabelStyle = {
 	fontFamily: 'Roboto, sans-serif',
 	fontSize: '12px',
 	fontStyle: 'normal',
 	fontWeight: '700',
 	lineHeight: '18px',
-	letterSpacing: '0.10000000149011612px',
+	letterSpacing: '0.1px',
 	textAlign: 'left',
+	color: '#374754',
 }; 
+
+export const priceLabelHighlightStyle = {
+	...priceLabelStyle,
+	color: '',
+};
--- src/styles/colors.js
+++ src/styles/colors.js
@@ -2,11 +2,15 @@ export const whiteBackground = '#FFFFFF';
 export const greyBackground = '#F4F4F4';
 export const darkBlueBackground = '#181949';
 export const lightBlueBackground = '#F3F9FF';
-export const pinkBackground = '#FFD1E8'; 
+export const lightPinkBackground = '#FFD1E8';
+export const pinkBackground = '#E6367E'; 
 
 export const lightGreyBorder = '#DEDEDE';
+export const pinkBorder = '#E6367E';
 
 export const darkBlueText = '#181949';
 export const lightBlueText = '#CFE5FF';
 export const whiteText = '#FFFFFF';
 export const greyText ='#666666';
+
+export const lightGreyBoxShadow = 'rgba(0, 0, 0, 0.07)';
