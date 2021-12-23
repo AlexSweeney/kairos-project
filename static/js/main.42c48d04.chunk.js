b3346ee1a4bd8ff18c3a371aa85177d04ebfc343
Alex Sweeney <alexsweeney@live.co.uk>
2021-12-23 20:35:32 +0700

    Updates

---
+++ static/js/main.42c48d04.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpkairos-project"]=this["webpackJsonpkairos-project"]||[]).push([[0],[function(o,s){console.log("index js loaded")}],[[0,1]]]);
+//# sourceMappingURL=main.42c48d04.chunk.js.map
